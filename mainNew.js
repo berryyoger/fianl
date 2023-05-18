59c2011ab88b0986a5e98af40b430a3240d7809f
berryyoger <berry.yoger@gmail.com>
2023-05-18 13:44:10 +0900

    진짜 모르겠다 어카지

--- mainNew.js
+++ mainNew.js
@@ -31,9 +31,36 @@ let upComingFromRight = new IntersectionObserver((e)=>{
     })
 })
 
+let cycle = 1
+let passDstnsSub = 0
+let passDstnsMain = 0
+let pass = () =>{
+    if(cycle<=3){
+        passDstnsMain = passDstnsMain-120
+        passDstnsSub = passDstnsSub-83
+        bennerTextGroupH1.style.transform = "translateY("+passDstnsMain+"px)"
+        bennerTextGroupH2.style.transform = "translateY("+passDstnsSub+"px)"
+        cycle++
+        if(cycle>3){
+            bennerTextGroupH1.style.transition = "none"
+            bennerTextGroupH2.style.transition = "none"
+            passDstnsMain = 120
+            passDstnsSub = 83
+            bennerTextGroupH1.style.transition = "all 2s"
+            bennerTextGroupH2.style.transition = "all 2s"
+            cycle = 0
+        }
+    }
+    else{
+    }
+    console.log(cycle)
+}
+
 let textBox = document.querySelectorAll('article')
 let left = document.querySelectorAll('.left')
 let right = document.querySelectorAll('.right')
+let bennerTextGroupH1 = document.querySelector('.bennerTextGroupH1')
+let bennerTextGroupH2 = document.querySelector('.bennerTextGroupH2')
 
     upComing.observe(textBox[0])
     upComing.observe(textBox[1])
@@ -49,3 +76,6 @@ let right = document.querySelectorAll('.right')
     upComingFromRight.observe(right[0])
     upComingFromRight.observe(right[1])
     upComingFromRight.observe(right[2])
+    
+
+    setInterval(pass, 4000)
